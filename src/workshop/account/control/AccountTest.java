acdb181a813ec2e27e02f8d6d9b4a025a7983d95
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-25 15:22:42 +0900

    AccountTest main

--- src/workshop/account/control/AccountTest.java
+++ src/workshop/account/control/AccountTest.java
@@ -1,7 +1,18 @@
 package workshop.account.control;
 
+import workshop.account.entity.Account;
+
 public class AccountTest {
 	public static void main(String[] args) {
+		Account account = new Account();
+		
+		//setter 메서드를 호출하여 Account 변수의 값을 변경 요청 
+		//고객번호 : "A1100", 계좌번호 : "221-22-3477", 잔액 : 100000
+		account.setCustId("A1100");
+		account.setAcctId("221-22-3477");
+		account.deposit(100000);
+		
+		System.out.println();
 		
 	}
 }
