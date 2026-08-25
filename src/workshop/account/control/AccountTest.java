0abf29fd499d60717271b31de88f238e0b98c54d
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-25 16:52:09 +0900

    InsufficientBalanceException add

--- src/workshop/account/control/AccountTest.java
+++ src/workshop/account/control/AccountTest.java
@@ -1,27 +1,32 @@
 package workshop.account.control;
 
 import workshop.account.entity.Account;
+import workshop.account.exception.InsufficientBalanceException;
 //ctrl + f11 java run 
 public class AccountTest {
 	public static void main(String[] args) {
-		Account account = new Account();
-		
-		//setter 메서드를 호출하여 Account 변수의 값을 변경 요청 
-		//고객번호 : "A1100", 계좌번호 : "221-22-3477", 잔액 : 100000
-		account.setCustId("A1100");
-		account.setAcctId("221-22-3477");
-		account.deposit(100000);
-		
-		System.out.println("고객번호 = " + account.getCustId());
-		System.out.println("계좌번호 = " + account.getAcctId());
-		System.out.println("잔액 = " + account.getBalance());
-		
-		
-		Account account2 = new Account("A1200","221-22-3488",20000);
-		//getClass().getName() + '@' + Integer.toHexString(hashCode())
-		//출금
-		account2.withdraw(500000);
-		System.out.println(account2.toString());
-		
+		//try - catch 구문
+		try {
+			Account account = new Account();
+			
+			//setter 메서드를 호출하여 Account 변수의 값을 변경 요청 
+			//고객번호 : "A1100", 계좌번호 : "221-22-3477", 잔액 : 100000
+			account.setCustId("A1100");
+			account.setAcctId("221-22-3477");
+			account.deposit(100000);
+			
+			System.out.println("고객번호 = " + account.getCustId());
+			System.out.println("계좌번호 = " + account.getAcctId());
+			System.out.println("잔액 = " + account.getBalance());
+			
+			
+			Account account2 = new Account("A1200","221-22-3488",20000);
+			//getClass().getName() + '@' + Integer.toHexString(hashCode())
+			//출금
+			account2.withdraw(500000);
+			System.out.println(account2.toString());
+		}catch(InsufficientBalanceException exp) {
+			System.out.println("Error 메시지 = " + exp.getMessage());
+		}
 	}
 }
--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -1,5 +1,7 @@
 package workshop.account.entity;
 
+import workshop.account.exception.InsufficientBalanceException;
+
 public class Account {
 	private String custId;
 	private String acctId;
@@ -48,7 +50,7 @@ public class Account {
 	}
 	
 	//출금
-	public void withdraw(int amount) {
+	public void withdraw(int amount) throws InsufficientBalanceException {
 		this.balance -= amount;
 	}
 	
