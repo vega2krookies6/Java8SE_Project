41930b19ef2b9ee702580cef258a7dbd6978ff69
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-25 14:49:24 +0900

    Account method add

--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -1,5 +1,42 @@
 package workshop.account.entity;
 
 public class Account {
+	private String custId;
+	private String acctId;
+	private int balance;
 
+	//getter method
+	public String getCustId() {
+		return custId;
+	}
+	//setter
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+
+	public String getAcctId() {
+		return acctId;
+	}
+
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+	
+	//입금
+	public void deposit(int amount) {
+		//this.balance = this.balance + amount;
+		this.balance += amount;
+	}
+	
+	//출금
+	public void withdraw(int amount) {
+		this.balance -= amount;
+	}
+	
+	
+	
 }
