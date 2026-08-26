477a9d8be98c21c63cc67aa4139845862ffd1b25
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-26 15:42:55 +0900

    oop goog package add

--- src/chapter5/oop/good/Manager.java
+++ src/chapter5/oop/good/Manager.java
@@ -1,13 +1,23 @@
 package chapter5.oop.good;
 /* 관리자 클래스 */
 public class Manager extends Employee {
+	//부서명
+	private String deptName;
+	
     public Manager (String name, double salary) {
-        this.name = name;
-        this.salary = salary;
+        super(name,salary);
     }
+    
+    public Manager (String name, double salary, String deptName) {
+    	this(name,salary);
+    	this.deptName = deptName;
+    }
+    
     
     public void manageSalary(double rate) {
         salary = salary+ salary*(rate/100);
         salary += 20; // 20만원을 추가로 받는다.
     }
+    
+    
 }
--- src/chapter5/oop/good/MereClerk.java
+++ src/chapter5/oop/good/MereClerk.java
@@ -3,11 +3,12 @@ package chapter5.oop.good;
 public class MereClerk extends Employee {
     
     public MereClerk (String name, double salary) {
-        this.name = name;
-        this.salary = salary;
+//        this.name = name;
+//        this.salary = salary;
+    	super(name,salary);
     }
     
     public void manageSalary(double rate) {
-        salary = salary+ salary*(rate/100);
+        salary = salary + salary*(rate/100);
     }
 }
