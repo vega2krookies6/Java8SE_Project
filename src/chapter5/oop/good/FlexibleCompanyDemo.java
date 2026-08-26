ae30e68069472e40d775a91728d8b76c007c0637
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-26 16:24:56 +0900

    oop good package add

--- src/chapter5/oop/good/FlexibleCompanyDemo.java
+++ src/chapter5/oop/good/FlexibleCompanyDemo.java
@@ -6,33 +6,48 @@ public class FlexibleCompanyDemo {
     	//Employee emp = new Employee();
     	Employee emp1 = new MereClerk("철수", 100);
     	Employee emp2 = new MereClerk("영희", 100);
+    	
     	Employee emp3 = new Manager("홍길동", 200,"인사부");
+    	System.out.println(emp3.getName());
+    	System.out.println(emp3.getSalary());
+    	
+    	Manager mgr = (Manager)emp3;
+    	System.out.println(mgr.getDeptName());
+    	
+    	Manager emp4 = new Manager("둘리", 200,"개발부");
+    	System.out.println(emp4.getName());
+    	System.out.println(emp4.getSalary());
+    	System.out.println(emp4.getDeptName());
+           	
+    	//Employee 타입인 배열을 선언  Heterogeneous Array
+    	Employee[] emps = new Employee[3];
+    	emps[0] = new MereClerk("철수", 100);
+    	emps[1] = new MereClerk("영희", 100);
+    	emps[2] = new Manager("홍길동", 200,"인사부");
         
-        MereClerk mereClerk1 = new MereClerk("철수", 100);
-        MereClerk mereClerk2 = new MereClerk("영희", 100);
-        Manager manager = new Manager("홍길동", 200);
+        
+        for(Employee emp: emps) {
+        	System.out.println("현재 월급입니다.");
+        	System.out.println(emp.getName() + "의 현재 월급은 " + emp.getSalary() + " 만원 입니다.");
+        	
+        	emp.manageSalary(10);
+        	
+            System.out.println("올린 후의 월급입니다.");
+            System.out.println(emp.getName() + "의 현재 월급은 " + emp.getSalary() + " 만원 입니다.");
+        }                
 
-        
-        
-        System.out.println("현재 월급입니다.");
-        
-        System.out.println(mereClerk1.getName() + "의 현재 월급은 " + mereClerk1.getSalary() + " 만원 입니다.");
-        System.out.println(mereClerk2.getName() + "의 현재 월급은 " + mereClerk2.getSalary() + " 만원 입니다.");
-        System.out.println(manager.getName() + "의 현재 월급은 " + manager.getSalary() + " 만원 입니다.");
-
-        System.out.println("");
-        
-        
-        System.out.println("올린 후의 월급입니다.");
-        
-        mereClerk1.manageSalary(10);
-        System.out.println(mereClerk1.getName() + "의 현재 월급은 " + mereClerk1.getSalary() + " 만원 입니다.");
-        
-        mereClerk2.manageSalary(10);
-        System.out.println(mereClerk2.getName() + "의 현재 월급은 " + mereClerk2.getSalary() + " 만원 입니다.");
-        
-        manager.manageSalary(10);
-        System.out.println(manager.getName() + "의 현재 월급은 " + manager.getSalary() + " 만원 입니다.");
+//        System.out.println("");        
+//        
+//        System.out.println("올린 후의 월급입니다.");
+//        
+//        mereClerk1.manageSalary(10);
+//        System.out.println(mereClerk1.getName() + "의 현재 월급은 " + mereClerk1.getSalary() + " 만원 입니다.");
+//        
+//        mereClerk2.manageSalary(10);
+//        System.out.println(mereClerk2.getName() + "의 현재 월급은 " + mereClerk2.getSalary() + " 만원 입니다.");
+//        
+//        manager.manageSalary(10);
+//        System.out.println(manager.getName() + "의 현재 월급은 " + manager.getSalary() + " 만원 입니다.");
         
    }
 }
